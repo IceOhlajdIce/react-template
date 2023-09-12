fc3bdea85c92f39e1ec1c19a0d6e1edebc7d23e4
IceOhlajdIce <iceohlajdice@mail.ru>
2023-09-12 14:39:07 +0300

    first commit

---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import pict from "./img/5.png"
+
+const Module = () => (
+    <>
+        <h1>Hello, React!</h1>
+        <img src={pict} />
+    </>
+);
+
+export default Module;
